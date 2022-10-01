e9cd047902a5ea595640913ee7e3c925b0a43dab
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-10-01 19:43:34 +0530

    Create xpshort.py

---
+++ xpshort.py
@@ -0,0 +1,50 @@
+import re
+import time
+import cloudscraper
+from bs4 import BeautifulSoup 
+print("Everything Looks Good! Lets Continue.")
+
+url = "https://techymozo.com/hgSq9"  #@param {type:"string"}
+
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+def bypass(url):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    
+    
+    DOMAIN = "https://push.bdnewsx.com"
+
+    url = url[:-1] if url[-1] == '/' else url
+
+    code = url.split("/")[-1]
+    
+    final_url = f"{DOMAIN}/{code}"
+    
+    r = client.get(url,timeout=5)
+
+    ref = re.findall("action[ ]{0,}=[ ]{0,}['|\"](.*?)['|\"]", r.text)[0]
+
+    h = {'referer': ref}
+
+    resp = client.get(final_url,headers = h)
+    
+    soup = BeautifulSoup(resp.content, "html.parser")
+    
+    try: inputs = soup.find(id="go-link").find_all(name="input")
+    except: return "Incorrect Link"
+    
+    data = { input.get('name'): input.get('value') for input in inputs }
+
+    h = { "x-requested-with": "XMLHttpRequest" }
+    
+    time.sleep(8)
+    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+    try:
+        return r.json()['url']
+    except: return "Something went wrong :("
+    
+# ---------------------------------------------------------------------------------------------------------------------
+print(bypass(url))
